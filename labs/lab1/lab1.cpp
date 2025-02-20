2eb51c81f2dfae1a12d5e459b3bf41f3c04dd483
NadiaDym <nadiadym592006@gmail.com>
2025-02-20 12:50:19 +0300

    были исправлены все ошибки
    верс 2

--- labs/lab1/lab1.cpp
+++ labs/lab1/lab1.cpp
@@ -7,25 +7,22 @@ int main()
     setlocale(LC_ALL, "RU");
     Stack<int> stack;
     int input = 1;
-    while (input != 0) 
-    {
-        cout << "Введите число последовательности: ";
-        cin >> input;
-        if (input != 0) 
-        {
-            stack.push(input);
-        }
-    }
-    cout << "Числа последовательности: " << endl;
-    while (stack.count()) 
-    {
-        if (stack.peek() > 0) 
-        {
-            cout << stack.pop() << endl;
-        }
-        else 
-        {
-            stack.pop();
-        }
-    }
+	while (input != 0) 
+	{
+		cout << "Введите число последовательности: ";
+		cin >> input;
+		if (input != 0) 
+		{
+			stack.push(input);
+		}
+	}
+	cout << "Положительные числа последовательности: " << endl;
+	while (stack.count()) 
+	{
+		int number = stack.pop();
+		if (number > 0) 
+		{
+			cout << number << endl;
+		}
+	}
 }
